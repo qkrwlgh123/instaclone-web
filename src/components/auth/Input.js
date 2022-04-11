53b10c94f7e8861391b0672485fd01f453ab3fba
qkrwlgh123 <jiho1024@naver.com>
2022-04-11 16:23:10 +0900

    button border-radius

--- src/components/auth/Input.js
+++ src/components/auth/Input.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const Input = styled.input`
   width: 100%;
-  border-radius: 3px;
+  border-radius: 5px;
   padding: 7px;
   background-color: #fafafa;
   border: 0.5px solid rgb(219, 219, 219);
